588f6c5f2dba10b1269a76976daaf4058f2b1bc3
Anthonyhill7474 <anthonyhill7474@gmail.com>
2025-11-27 01:46:06 +1100

    init file for scraper

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -1,9 +1,9 @@
 """scraper package init"""
 
 
-from .selenium_scraper import scrape_liftingcast_roster
-from .playwright_scraper import scrape_playwright_sync
-from .utils import lifter_link_selector
+from .selenium_scraper import scrape_liftingcast_roster, get_driver
+from .playwright_scraper import scrape_playwright_sync, scrape_playwright_sync
+from .utils import lifter_link_selector, slugify, wait_for_lifters_condition, clean_lifter_name
 
 
-__all__ = ["scrape_liftingcast_roster", "scrape_playwright_sync", "lifter_link_selector"]
+__all__ = ["scrape_with_playwright", "scrape_playwright_sync", "lifter_link_selector", "scrape_liftingcast_roster", "slugify", "get_driver", "wait_for_lifters_condition", "save_html_report", "clean_lifter_name"]
