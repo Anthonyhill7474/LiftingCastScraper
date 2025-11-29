079bfead0e5a377cc0e9da49cf440a6292ed7f90
Anthonyhill7474 <anthonyhill7474@gmail.com>
2025-11-29 15:13:26 +1100

    moved files to under new /src folder for further extensibility

--- reports/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Reports package init"""
-
-
-from .html_report import generate_html_report
-
-
-__all__ = ["generate_html_report"]
--- scraper/__init__.py
+++
@@ -1,8 +0,0 @@
-"""scraper package init"""
-
-
-from .selenium_scraper import scrape_liftingcast_roster, get_driver
-from .utils import lifter_link_selector, slugify, wait_for_lifters_condition, clean_lifter_name
-
-
-__all__ = ["lifter_link_selector", "scrape_liftingcast_roster", "slugify", "get_driver", "wait_for_lifters_condition", "save_html_report", "clean_lifter_name"]
